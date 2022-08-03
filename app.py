0bc3ac4f7007cd9983c70e4aa97b9f3a6013314c
Ahmet Sari <106359636+eeeng@users.noreply.github.com>
2022-08-03 11:47:16 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+import time
+
+app=Flask(__name__)
+
+app.route("/")
+def Hello_world():
+    return jsonify({"Time to call": time.time()})
+if __name__=="__main__":
+    app.run(host='0.0.0.0', debug=True)
+    
